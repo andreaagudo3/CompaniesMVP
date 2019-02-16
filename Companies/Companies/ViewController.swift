3aa00594ccce7631a0c16958dd303c61917489ce
Andrea Agudo <andreaagudo3@gmail.com>
2019-02-16 13:53:16 +0100

    initial project + pods + swiftlint

---
+++ Companies/Companies/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Companies
+//
+//  Created by Andrea Agudo on 16/02/2019.
+//  Copyright © 2019 aagudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
