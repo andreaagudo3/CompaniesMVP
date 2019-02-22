a93773c1a297068483068f5972861ef2e471e9bc
Andrea Agudo <andreaagudo3@gmail.com>
2019-02-22 19:14:21 +0100

    companyCell

--- Companies/Companies/CompaniesList/View/cell/CompanyCell.swift
+++ Companies/Companies/CompaniesList/View/cell/CompanyCell.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class CompanyCell: UITableViewCell {
+final class CompanyCell: UITableViewCell {
 
+    @IBOutlet weak var customizedView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    static let identifier = "companyCell"
+    static let nibName = "CompanyCell"
+
+    var company: Company? {
+        didSet {
+            setData()
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        initView()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func initView() {
+        customizedView.setCustomizedShadow()
+    }
 
-        // Configure the view for the selected state
+    private func setData() {
+        guard let company = self.company else { return }
+
+        if let name =  company.name {
+            titleLabel.text = name
+        }
+        if let price = company.sharePrice {
+            priceLabel.text = price.description + "€"
+        }
     }
 
 }
